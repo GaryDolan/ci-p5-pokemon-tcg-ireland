df2d56c3bba07585f532bbcc5cd58a0b3eb27725
GaryDolan <garydolan4@gmail.com>
2024-03-26 11:13:04 +0000

    Add filtering by category and on sale #34 #44

--- products/views.py
+++ products/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
     """A function based view for displaying all products, including sorts and searches."""
 
     products = Product.objects.all()
     query = None
+    categories = None
+    on_sale = None
 
     if request.GET:
+        # Filter by category
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            # Capture catagories objects in category list so we can access their fields in the template
+            categories = Category.objects.filter(name__in=categories)
+        
+        # Filter by items on sale
+        if 'on_sale' in request.GET:
+            products = Product.objects.filter(on_sale=True)
+            # check if 1 in template and if so display SALE in place of category name
+            on_sale = 1
+
+        # Search in name or description
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,6 +37,8 @@ def all_products(request):
     context = {
         'products':products,
         'search_term': query,
+        'current_categories': categories,
+        'on_sale': on_sale,
     }
 
     return render(request, 'products.html', context)
