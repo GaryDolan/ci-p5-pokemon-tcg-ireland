aecc34b655b34c21fdbf31fc7f39b118382722a8
GaryDolan <garydolan4@gmail.com>
2024-03-28 04:05:46 +0000

    Add new and popular products sections to home #72

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
     """A function based view for displaying the home page."""
 
-    return render(request, 'index.html')
+    new_products = Product.objects.order_by('-date_added')[:4]
+    popular_products = Product.objects.order_by('-rating')[:4]
+
+    context = {
+        'new_products': new_products,
+        'popular_products': popular_products,
+    }
+
+    return render(request, 'index.html', context)
--- products/views.py
+++ products/views.py
@@ -79,7 +79,7 @@ def all_products(request):
     current_sorting = f'{sort}_{direction}'
 
     context = {
-        'products':products,
+        'products': products,
         'search_term': query,
         'current_categories': categories,
         'on_sale': on_sale,
