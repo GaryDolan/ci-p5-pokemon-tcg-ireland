e0df109074f88ce6cb822b66b17528d974261a6c
GaryDolan <garydolan4@gmail.com>
2024-03-30 01:39:36 +0000

    Add add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-from products.models import Product
+from django.shortcuts import render, redirect
 
 def cart_view(request):
     """A function based view for displaying the home page."""
 
 
     return render(request, 'cart.html')
+
+def add_to_cart(request, item_id):
+
+    quantity = int (request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url') 
+
+    # use request response cycle session to store cart contents
+    # if no cart create empty dict as cart
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
