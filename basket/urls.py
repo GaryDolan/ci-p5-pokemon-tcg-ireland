54acc1c0d19280541c0061e0d5262d2d518a7e6e
GaryDolan <garydolan4@gmail.com>
2024-04-01 00:42:13 +0100

    Add quantity selection to basket #51

--- basket/urls.py
+++ basket/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.basket_view, name='basket_view'),
+    path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
+
 
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
-def basket_view(request):
+def view_basket(request):
     """A function based view for displaying the home page."""
 
 
@@ -22,3 +22,17 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+
+    quantity = int (request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
