bdf76d6bf40313d02030e57fdfc87a40e69a14cd
GaryDolan <garydolan4@gmail.com>
2024-04-03 03:41:37 +0100

    Add initial checkout functionality #53

--- checkout/views.py
+++ checkout/views.py
@@ -3,21 +3,39 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from basket.context import basket_contents
 
+import stripe
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your basket is empty")
         return redirect(reverse('products'))
+    
+    # Use basket context function to retrieve basket total for stripe 
+    current_basket = basket_contents(request)
+    total = current_basket['basket_total']
+    # Stripe needs an int
+    stripe_total = round (total *100)
+    # Set secret key on stripe
+    stripe.api_key = stripe_secret_key
+    # Create payment intent 
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test client secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
