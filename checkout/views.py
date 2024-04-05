bb6801870808da6da00e0f4367d0dfdb9ddc7f34
GaryDolan <garydolan4@gmail.com>
2024-04-05 15:17:40 +0100

    Prepopulate checkout form, save info functionality

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from basket.context import basket_contents
 
 import stripe
@@ -54,6 +56,7 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
+
         # Create instance of form using submitted data
         order_form = OrderForm(form_data)
 
@@ -88,7 +91,7 @@ def checkout(request):
             messages.error(request, 'There was an error with your form. \
                             Please double check your information.')
 
-    # Request is a GET
+    # Request is a GET so populate form and display
     else:
         # Return to products if basket empty
         basket = request.session.get('basket', {})
@@ -109,7 +112,26 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-    order_form = OrderForm()
+         # If user has data saved populated the form with it
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+
     template = 'checkout.html'
     context = {
         'order_form': order_form,
@@ -127,6 +149,30 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     # get order details of order that just succeeded and called this view
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            # Populates form with both data from profile (instance=profile) and 
+            # user entered data, allows users to view current and changed data (profile_data)
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
