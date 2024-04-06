f71b2fd3e7e95cb80bec6e4b4b6af73e15e8f161
GaryDolan <garydolan4@gmail.com>
2024-04-06 02:26:25 +0100

    Add user acc & password editing to profile #26

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,12 +1,19 @@
 from django import forms
+
+from django.contrib.auth.models import User
+
 from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
-    """
-    Form for checkout to collect user info.
-    """
+    """ Form for updating user profile"""
 
     class Meta:
         model = UserProfile
         exclude = ('user',)
+
+class UserAccountForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name',)
+        
--- profiles/views.py
+++ profiles/views.py
@@ -1,39 +1,96 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
-from .forms import UserProfileForm
+from .forms import UserProfileForm, UserAccountForm
 
 from checkout.models import Order
 
-
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    # Vars to track which form failed
+    acc_form_failed = False
+    profile_form_failed = False
+    password_form_failed = False
+
     if request.method == 'POST':
-        if 'edit_info' in request.POST:
+        if 'edit_acc_info' in request.POST:
+            user_account_form = UserAccountForm(request.POST, instance=request.user)
+            if user_account_form.is_valid():
+                user_account_form.save()
+                messages.success(request, 'Account info updated successfully')
+            else:
+                messages.error(request, 'An error occurred updating your info, please try again')
+                acc_form_failed = True
+        elif 'edit_info' in request.POST:
             # Create a new instance of the UserProfileform using the post data
             # Update the instance of the profile we retrieved above
-            form = UserProfileForm(request.POST, instance=profile)
-            if form.is_valid():
-                form.save()
-                messages.success(request, 'Profile updated successfully')
+            user_profile_form = UserProfileForm(request.POST, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+                messages.success(request, 'Shipping info updated successfully')
+            else:
+                messages.error(request, 'An error occurred updating your shipping info, please try again')
+                profile_form_failed = True
+        elif 'edit_password' in request.POST:
+            # Password change form uses user not instanc as parameter
+            user_password_form = PasswordChangeForm(request.user, request.POST)
+            if user_password_form.is_valid():
+                user_password_form.save()
+                messages.success(request, 'Password updated successfully')
+            else:
+                messages.error(request, 'An error occurred updating your password, please try again')
+                password_form_failed = True
+        
+        # Render the template only initialising forms that have not failed validation.
+        # If we failed a form submission return the same instance of the form to display error in validation.
+        user_account_form = UserAccountForm(instance=request.user) if not acc_form_failed else user_account_form
+        user_profile_form = UserProfileForm(instance=profile) if not profile_form_failed else user_profile_form
+        user_password_form = PasswordChangeForm(request.user) if not password_form_failed else user_password_form
 
-    # Populate the form with current user information
-    form = UserProfileForm(instance=profile)
-    # Get all user orders
-    orders = profile.orders.all()
+        # Get all user orders
+        orders = profile.orders.all()
 
-    template = 'profile.html'
-    context = {
-        'form': form,
-        'orders': orders,
-        'on_profile_page': True
-    }
+        template = 'profile.html'
+        context = {
+            'user_account_form': user_account_form,
+            'user_profile_form': user_profile_form,
+            'user_password_form': user_password_form,
+            'orders': orders,
+            'on_profile_page': True,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
+            
+                
+    else:
+        # GET request so just display the page
+        # Populate the forms with current user information
+        user_account_form = UserAccountForm(instance=request.user)
+        user_profile_form = UserProfileForm(instance=profile)
+        user_password_form = PasswordChangeForm(request.user)
 
+
+        # Get all user orders
+        orders = profile.orders.all()
+
+        template = 'profile.html'
+        context = {
+            'user_account_form': user_account_form,
+            'user_profile_form': user_profile_form,
+            'user_password_form': user_password_form,
+            'orders': orders,
+            'on_profile_page': True
+        }
+
+        return render(request, template, context)
+
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
