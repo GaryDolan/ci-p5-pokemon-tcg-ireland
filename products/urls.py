1564732c30229a3abc41e30a6b4c8669fce6f8d4
GaryDolan <garydolan4@gmail.com>
2024-04-07 23:26:00 +0100

    Add template, view and url for edit product #39

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # if we didnt add would match this and because its first in the list it would be called
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
-
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    
 ]
