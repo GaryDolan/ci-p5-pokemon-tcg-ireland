4b5d0178c980a4a105ca15eb7c14d7ccb34ddb33
GaryDolan <garydolan4@gmail.com>
2024-04-15 13:41:23 +0100

    Add profiles to admin panel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ['user', 'default_phone_number', 'default_street_address1', 'default_street_address2', 'default_town_or_city', 'default_country', 'default_postcode', 'default_county']
+    list_filter = ['default_town_or_city', 'default_county', 'default_country']
+    search_fields = ['user', 'default_phone_number']
