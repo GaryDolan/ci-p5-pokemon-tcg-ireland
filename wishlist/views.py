5137459014cee48c06ed540ead638b46245b855f
GaryDolan <garydolan4@gmail.com>
2024-04-17 22:58:42 +0100

    Add wishlist toggle view #36

---
+++ wishlist/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from products.models import Product
+from .models import Wishlist
+
+@login_required
+def wishlist_toggle(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    user_profile = request.user.userprofile
+
+    wishlist_item_exists = Wishlist.objects.filter(user_profile=user_profile, products=product).exists()
+    
+    # Add or remove the product from wishlist base on if it exists
+    if wishlist_item_exists:
+        wishlist_item = Wishlist.objects.get(user_profile=user_profile, products=product)
+        wishlist_item.products.remove(product)
+        messages.success(request, f'{product.name} has been removed from your wishlist')
+    else:
+        wishlist = Wishlist.objects.get(user_profile=user_profile)
+        wishlist.products.add(product)
+        wishlist.save()
+        messages.success(request, f'{product.name} has been added to your wishlist')
+
+    return redirect(reverse('product_detail', args=[product_id]))
+
