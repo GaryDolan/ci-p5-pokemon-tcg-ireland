4ecea916ab2fd720014472ade0b7cbb69504ffce
GaryDolan <garydolan4@gmail.com>
2024-04-17 22:50:56 +0100

    Add wishlist to admin panel #36

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
 
+from .models import Wishlist
+
+class WishlistProductInline(admin.TabularInline):
+    model = Wishlist.products.through
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    inlines = [WishlistProductInline,]
+
+    list_display = ['user_profile',]
