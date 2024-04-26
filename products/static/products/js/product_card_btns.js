5acde82c61ceedb2ec8057c2172a94068d354f82
GaryDolan <garydolan4@gmail.com>
2024-04-26 02:44:50 +0100

    Add js to enable/disable submit btn on product card

---
+++ products/static/products/js/product_card_btns.js
@@ -0,0 +1,25 @@
+function enableButtons() {
+    // Enable all buttons with the class add_to_basket_btn
+    var buttons = document.getElementsByClassName("add-btn");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = false;
+    }
+}  
+
+function disableButtons() {
+    // Disable all buttons with the class add_to_basket_btn
+    var buttons = document.getElementsByClassName("add-btn");
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+    }
+}
+
+window.onload = function() {
+    enableButtons();
+    var forms = document.getElementsByClassName("add-product-form");
+    for (var i = 0; i < forms.length; i++) {
+        forms[i].addEventListener("submit", function() {
+            disableButtons();
+        });
+    }
+}
