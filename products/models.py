db13dad77909aeae742e94593e35930fd091e447
GaryDolan <garydolan4@gmail.com>
2024-04-27 19:46:11 +0100

    Update pre sale price field so it can be blank

--- products/models.py
+++ products/models.py
@@ -49,7 +49,7 @@ class Product(models.Model):
     description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    pre_sale_price = models.DecimalField(max_digits=6, decimal_places=2)
+    pre_sale_price = models.DecimalField(max_digits=6, decimal_places=2 , null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
